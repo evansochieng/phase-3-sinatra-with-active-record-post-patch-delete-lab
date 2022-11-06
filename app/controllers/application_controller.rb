148da055a4dec8d8a9fca3393b88c3e6b0b76e24
Evans Ochieng <evansochieng121@gmail.com>
2022-11-06 19:57:25 +0300

    practice performing crud operations from sinatra controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < Sinatra::Base
   set default_content_type: "application/json"
   
+  #GET requests
   get '/bakeries' do
     bakeries = Bakery.all
     bakeries.to_json
@@ -20,6 +21,46 @@ class ApplicationController < Sinatra::Base
   get '/baked_goods/most_expensive' do
     # see the BakedGood class for the  method definition of `.by_price`
     baked_good = BakedGood.by_price.first
+    baked_good.to_json
+  end
+
+  #POST requests
+  #POST /baked_goods
+  post '/baked_goods' do
+    #create a new baked_good in db
+    baked_good = BakedGood.create(
+      name: params[:name],
+      price: params[:price],
+      bakery_id: params[:bakery_id]
+    )
+
+    #return the created baked_good as JSON
+    baked_good.to_json
+  end
+
+  #PATCH requests
+  #PATCH /bakeries/:id
+  patch '/bakeries/:id' do
+    #get the bakery whose id matches the passed id
+    bakery = Bakery.find(params[:id])
+
+    #update the name of the bakery
+    bakery.update(name: params[:name])
+
+    #return the updated bakery as JSON
+    bakery.to_json
+  end
+
+  #DELETE request
+  #DELETE /baked_goods/:id
+  delete '/baked_goods/:id' do
+    #find the baked_good to delete
+    baked_good = BakedGood.find(params[:id])
+
+    #delete the baked_good
+    baked_good.destroy
+
+    #return the deleted baked_good as JSON
     baked_good.to_json
   end
 
